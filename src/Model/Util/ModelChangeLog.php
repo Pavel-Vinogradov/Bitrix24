0194d8514661855e175164260bdc64851acf21b4
Pavel Vinogradov <pavelvinogradov45@gmail.com>
2024-01-24 13:42:44 +0300

    v2

--- src/Model/Util/ModelChangeLog.php
+++ src/Model/Util/ModelChangeLog.php
@@ -17,7 +17,7 @@ use Illuminate\Database\Eloquent\Model;
  * @property string|null $new_value
  * @property Carbon|string $created_at
  */
-final class ModelChangeLog extends Model
+class ModelChangeLog extends Model
 {
     protected $table = 'util.model_change_log';
 
